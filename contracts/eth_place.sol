7ae00035b6a36fa63084250a9a99171f30890824
Ben Eustace <krubot.ops@gmail.com>
2022-04-23 20:28:17 +0100

    Add further documentation for images

--- contracts/eth_place.sol
+++ contracts/eth_place.sol
@@ -37,7 +37,7 @@ contract Places {
 
     function store(Colour _colour, uint i, uint j) public payable {
         require(msg.value >= price, "You need to send more than the current base");
-        price = price + 1000000000000;
+        price = price + 1000000;
         state[i][j] = _colour;
     }
 
